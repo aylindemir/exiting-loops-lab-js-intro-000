a4009be4fc9b97907d2c6b5242451ff6f8b08a53
Aylin Demir <aylin.de@outlook.com>
2020-01-07 02:45:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function breakOut(array, changeValue, stopValue){
+  for(let i =0;array.length;i++){
+    // if(array[i] !== stopValue){
+        array[i]= changeValue
+    // }
+  }
+  return array
+}
